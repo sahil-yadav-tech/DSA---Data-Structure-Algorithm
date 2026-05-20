9c9559cc81c13c8adbc0c5bfedbc775a558cbc14
Sahil Yadav <sahil9082811@gmail.com>
2026-05-20 23:36:50 +0530

    Solving inserction problem

--- index.js
+++ index.js
@@ -27,3 +27,33 @@ for (let index = 0; index < array1.length; index++) {
 }
 
 console.log(intersection, "intersection");
+
+
+
+const array1 = [1, 8, 9, 8, 14];
+const array2 = [2, 8];
+
+let intersection = [];
+
+for (let index = 0; index < array1.length; index++) {
+  if (array2.includes(array1[index]) && !intersection.includes(array1[index])) {
+    intersection.push(array1[index]);
+  }
+}
+
+console.log(intersection, "intersection");
+
+
+//! Implement an algorithm to find the intersection of two arrays.
+const array1 = [1, 8, 9, 8, 14];
+const array2 = [2, 8];
+
+let intersection = [];
+
+for (let index = 0; index < array1.length; index++) {
+  if (array2.includes(array1[index]) && !intersection.includes(array1[index])) {
+    intersection.push(array1[index]);
+  }
+}
+
+console.log(intersection, "intersection");
