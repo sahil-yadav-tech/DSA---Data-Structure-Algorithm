6ddc1fc30c4b628a12382153055df223ec7b78ba
Sahil Yadav <sahil9082811@gmail.com>
2026-05-31 20:07:54 +0530

    solved largest and smallest value in array

--- basics/arrays/04-largest-number.js
+++ basics/arrays/04-largest-number.js
@@ -1,6 +1,6 @@
 const array = [1, 0, 2, 3, 0, 4, 5, 0, 8];
 let maxValue = array[0];
-
+let minValue = array[0];
 
 const largestNumber = (arr) => {
   console.log("Original array:", arr);
@@ -12,3 +12,16 @@ const largestNumber = (arr) => {
   console.log("Largest number:", maxValue);
 };
 largestNumber(array);
+
+
+const smallestNumber = (arr) => {
+  console.log("Original array:", arr);
+  let minValue = arr[0];    
+    for(let i=1; i<arr.length; i++) {   
+        if(arr[i]< minValue) {
+            minValue = arr[i];
+        }   
+    }
+    console.log("Smallest number:", minValue);
+}
+smallestNumber(array)
