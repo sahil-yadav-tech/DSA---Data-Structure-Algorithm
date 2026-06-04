03c2685cd9583643bcafc2a6d14f7cb69dc68d79
Sahil Yadav <sahil9082811@gmail.com>
2026-06-04 20:53:19 +0530

    check validation and break in linear serach

--- basics/arrays/11-linear-serach.js
+++ basics/arrays/11-linear-serach.js
@@ -1,12 +1,16 @@
-const array = [5,10,15,20,]
-let target = 15
+const array = [5, 10, 15, 20];
+let target = 15;
+
+let found = false;
 
 for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    if(array[i] === target){
-        console.log("target value is one:", i , "index");
-        
-    }else{
-        console.log("Not found proper Index");
+    if (array[i] === target) {
+        console.log("Target found at index:", i);
+        found = true;
+        break;
     }
+}
+
+if (!found) {
+    console.log("Target not found");
 }
