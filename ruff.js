12d0d1e10d3ecc5f051a876209dca4428142cc94
Sahil Yadav <sahil9082811@gmail.com>
2026-07-11 23:54:40 +0530

    prcaticing maxDiffernce practcing

--- ruff.js
+++ ruff.js
@@ -1,15 +1,19 @@
 const arrayValue = [41, 25, 63, 56, 45, 100, 10, 500];
 
-const checkEvenOrOdd = (array) => {
+const maxDifference = (array) => {
+  let maxDifferenceIs = -Infinity;
+  
   for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-    if(element%2 ===0){
-      console.log(element, "Its Even Number");
-    }else{
-      console.log(element, "Its Odd Number");
+    for (let j = 0; j < array.length; j++) {
+      let difference = array[i] - array[j];
+      if (difference > maxDifferenceIs) {
+        maxDifferenceIs = difference;
+      }
     }
   }
+  
+  return maxDifferenceIs;  // ✅ Return the calculated value
 };
 
-console.log("The Avg of array is ", checkEvenOrOdd(arrayValue));
+console.log("The maxDifference of array is ", maxDifference(arrayValue));
+// Output: 490 (500 - 10)
