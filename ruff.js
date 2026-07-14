b446173ebeef96a638dacff552dc3c282eb1adc7
Sahil Yadav <sahil9082811@gmail.com>
2026-07-14 22:42:55 +0530

    moved array to last

--- ruff.js
+++ ruff.js
@@ -1,13 +1,21 @@
-const array = [1, 2, 3, 4, 5, 8, 15.2];
+const array = [41, 25, 63, 0, 0, 56, 0, 0, 41, 45, 45, 100, 0, 0, 0, 0];
+let newArray = [];
+let zeroCount = 0;
+for (let index = 0; index < array.length; index++) {
+  const element = array[index];
+  if (!(element === 0)) {
+    newArray.push(element);
+  } else {
+    zeroCount++;
+  }
 
-for (let i = 0; i < array.length - 1; i++) {
-    let current = array[i];
-    let next = array[i + 1];
-
-
-    while (next - current> 1) {
-         console.log("Missing Number:", current + 1);
-        current++;
-    }
 
 }
+
+for (let i = 0; i < zeroCount; i++) {
+  newArray.push(0);
+}
+
+console.log(zeroCount);
+console.log(newArray, "newArray");
+
