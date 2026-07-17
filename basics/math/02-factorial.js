c9ed997a04d6f25ac8db83ab211164851bcc8308
Sahil Yadav <sahil9082811@gmail.com>
2026-07-17 22:55:19 +0530

    adding factorial sum solution

--- basics/math/02-factorial.js
+++ basics/math/02-factorial.js
@@ -1,15 +1,38 @@
-const factorial = (number) => {
-  // console.log(number, "Number");
-  let result = 1;
+// const factorial = (number) => {
+//   // console.log(number, "Number");
+//   let result = 1;
 
-  for (let i = result; i <= number; i++) {
-    console.log(i);
-    result = result * i;
+//   for (let i = result; i <= number; i++) {
+//     console.log(i);
+//     result = result * i;
+//   }
+
+//   console.log(result, "result");
+// };
+
+// factorial(4);
+
+// TODO USING RECUSERSION
+
+const factorial = (number) => {
+  if (number === 0 || number === 1) {
+    return 1;
   }
 
-  console.log(result, "result");
+  return number * factorial(number - 1);
 };
 
-factorial(4);
+console.log(factorial(4)); // 24
 
-// TODO USING RECUSERSION 
+// const printCount = (number) => {
+//     console.log(number, "number");
+    
+//   if (number === 20) {
+//     console.log("done");
+//     return;
+//   }
+
+//   printCount(number + 1);
+// };
+
+// printCount(5);
