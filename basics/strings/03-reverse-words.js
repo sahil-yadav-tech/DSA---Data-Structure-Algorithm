bb96136086c84ed38a263c1406fb343df70b5b72
Sahil Yadav <sahil9082811@gmail.com>
2026-07-20 23:20:05 +0530

    Solved revser the string on place

--- basics/strings/03-reverse-words.js
+++ basics/strings/03-reverse-words.js
@@ -2,19 +2,31 @@
 // Input: "hey There" → Output: "yeh erehT"
 // Input: "Hello World" → Output: "olleH dlroW"
 
-const reverseWords = (string) => {
-  // Split the string into an array of words
-  const words = string.split(" ");
-  
-  // Reverse each word individually
-  const reversedWords = words.map(word => {
-    return word.split("").reverse().join("");
-  });
-  
-  // Join the reversed words back together with spaces
-  return reversedWords.join(" ");
-};
+//TODO:- METHOD ONE
+// const reverseWords = (string) => {
+//   // Split the string into an array of words
+//   const words = string.split(" ");
 
-console.log(reverseWords("hey There")); // "yeh erehT"
-console.log(reverseWords("Hello World")); // "olleH dlroW"
-console.log(reverseWords("JavaScript is awesome")); // "tpircSavaJ si emosewa"
+//   // Reverse each word individually
+//   const reversedWords = words.map(word => {
+//     return word.split("").reverse().join("");
+//   });
+
+//   // Join the reversed words back together with spaces
+//   return reversedWords.join(" ");
+// };
+
+// console.log(reverseWords("hey There")); // "yeh erehT"
+// console.log(reverseWords("Hello World")); // "olleH dlroW"
+// console.log(reverseWords("JavaScript is awesome")); // "tpircSavaJ si emosewa"
+
+//TODO:- METHOD TWO
+
+const reverseWordsSecond = (string) => {
+  const data = string.split(" ").map((elem) => {
+    const values = elem.split("").reverse().join("");
+    return values;
+  }).join(" ")
+   return data
+};
+console.log(reverseWordsSecond("hey There"));
