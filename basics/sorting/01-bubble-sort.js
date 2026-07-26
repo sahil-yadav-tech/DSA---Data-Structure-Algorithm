d356fc1744f7254d60bc1b63655687f66681ea4c
Sahil Yadav <sahil9082811@gmail.com>
2026-07-26 11:53:27 +0530

    Bubble Sort

---
+++ basics/sorting/01-bubble-sort.js
@@ -0,0 +1,11 @@
+const array = [45, 55, 25, 10, 98, 87, 10];
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    for (let j = 0; j < array.length; j++) {
+        console.log(array[i], array[j]);
+        if(array[i]> array[j+1]){
+
+        }
+    }
+}
