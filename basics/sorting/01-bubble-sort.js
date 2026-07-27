7a0f5a8779b590f1f91131cf32aa688f91d27af5
Sahil Yadav <sahil9082811@gmail.com>
2026-07-27 22:57:59 +0530

    adding new bubble sort video

--- basics/sorting/01-bubble-sort.js
+++ basics/sorting/01-bubble-sort.js
@@ -1,11 +1,16 @@
 const array = [45, 55, 25, 10, 98, 87, 10];
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    for (let j = 0; j < array.length; j++) {
-        console.log(array[i], array[j]);
-        if(array[i]> array[j+1]){
-
+for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+        console.log(`Comparing: ${array[j]} and ${array[j+1]}`);
+        if (array[j] > array[j + 1]) {
+            // Swap elements
+            let temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+            console.log(`Swapped: ${array[j]} and ${array[j+1]}`);
         }
     }
 }
+
+console.log("Sorted array:", array);
