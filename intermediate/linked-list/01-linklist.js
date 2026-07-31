f324f8c8671a28c761a01cdde07c4a35d7e544ca
Sahil Yadav <sahil9082811@gmail.com>
2026-07-31 16:36:37 +0530

    starting link list

---
+++ intermediate/linked-list/01-linklist.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+
+    // Empty list
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.next) {
+      current = current.next;
+    }
+
+    current.next = newNode;
+  }
+}
+
+const list = new LinkedList();
+
+list.append(10);
+list.append(20);
+list.append(30);
+
+console.log(list);
--- ruff.js
+++ ruff.js
@@ -1,9 +1,12 @@
-const obj1 = {
-  name: "Sahil"
-};
+const array = [45, 55, 25, 10, 98, 87, 11];
+const target = 100;
 
-console.log(Object.keys(obj1));
-console.log(Object.values(obj1));
-console.log(Object.entries(obj1));
+for (let i = 0; i < array.length; i++) {
+  for (let j = 0; j < array.length; j++) {
+    if (array[i] + array[j] === target) {
+      console.log(array[i] + array[j] === target, "Founded ");
 
-
+      break;
+    }
+  }
+}
