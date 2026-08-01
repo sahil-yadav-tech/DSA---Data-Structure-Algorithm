3450cb0b748a9b5f08263cb9500c39749b167d95
Sahil Yadav <sahil9082811@gmail.com>
2026-08-01 17:55:46 +0530

    ädding new array Object

---
+++ tricky/ArrayObjectMethod.js
@@ -0,0 +1,30 @@
+// FOR IN LOOP
+// const user = {
+//     name:"sahil",
+//     age:"Hey"
+// }
+
+// for(let key in user){
+//     console.log(user[key]);
+// }
+
+// =================================================
+
+// let userArray = [445,5,9,]
+// let userString= "sahilyadav"
+
+// for(let key of userString){
+//     console.log(key, "key");
+    
+// }
+
+
+var myFriends = ["vinod", "ramesh", "arjun", "vishal"];
+myFriends.forEach(function (element, index, array) {
+  console.log(element + " index : " + index + " " + array);
+});
+
+
+
+
+
