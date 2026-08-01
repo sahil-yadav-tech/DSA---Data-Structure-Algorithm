5e15058c9a42b863cffb591cbe96a72d082e0400
Sahil Yadav <sahil9082811@gmail.com>
2026-08-01 18:09:20 +0530

    Checking array build in methods test

---
+++ tricky/FilterFuncrion.js
@@ -0,0 +1,2 @@
+const numbers = [4, 8, 9, 5];
+
---
+++ tricky/MapFunction.js
@@ -0,0 +1,20 @@
+//! Double all numbers in an array using map().
+const numbers = [4, 8, 9, 5];
+const DoubleNumber = numbers.map((element) => {
+  return element * 2;
+});
+console.log(DoubleNumber, "DoubleNumber");
+
+//!Convert an array of strings to uppercase usisng map().
+const arrayStrings  = ["cat", "dogs", "lion","elephant"] 
+const DoubleNumber = numbers.map((element) => {
+  return element * 2;
+});
+// Add a new property to every object in an array using map().
+// Extract only names from an array of user objects using map().
+// Convert an array of prices into prices with GST added.
+// Convert an array of numbers into their squares.
+// Convert an array of dates into formatted date strings.
+// Convert an array of strings into objects.
+// Use map() to generate HTML elements from data.
+// Convert an array of user objects into an array of IDs.
