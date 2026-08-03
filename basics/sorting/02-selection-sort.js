efd2e98d8f4949de44a937421144c85e53f4cdbe
Sahil Yadav <sahil9082811@gmail.com>
2026-08-04 00:28:03 +0530

    Solving and understanding th selction short

--- basics/sorting/02-selection-sort.js
+++ basics/sorting/02-selection-sort.js
@@ -1,4 +1,28 @@
-// TODO:-Selection Sort repeatedly finds the smallest element from the unsorted part of the array and places it at the beginning.
+// TODO:-Selection Sort repeatedly selects the smallest element from the unsorted part of the array and places it at its correct position.
+
+// "Selection Sort always finds the smallest element from the unsorted part of the array, places it in the correct position, and ignores the already sorted part in the next rounds."
+
+// NOTE 
+// STEPS
+// Answer: Store the index.
+// Why?
+// Because after we finish searching, we need to swap the first element with the smallest element.
+
+
+
+
+
+// Suppose we have:[29, 10, 14, 37, 13]
+// We found the smallest value:10
+// If we only store:smallest = 10
+// Now I ask you: Where is 10 in the array?
+
+//! You don't know! To swap, we need to know which position it is at.
+
+//! We store the index of the smallest element because swapping is done using array positions (indices), not just values.
+
+// We store the index of the smallest element because swapping is done using array positions (indices), not just values.
+
 
 const array = [45, 55, 25, 10, 98, 87, 10];
 
--- ruff.js
+++ ruff.js
@@ -1,13 +1,22 @@
 const array = [100, 45, 55, 25, 10, 98, 87, 11];
 
-for (let i = 0; i < array.length; i++) {
-  for (let j = 0; j < array.length - i; j++) {
-    if (array[j] > array[j + 1]) {
-      let temp = array[j];
-      array[j] = array[j + 1];
-      array[j + 1] = temp;
+for (let i = 0; i < array.length - 1; i++) {
+  let minIndex = i;
+  // console.log(minIndex, "min");
+
+  for (let j = i + 1; j < array.length; j++) {
+    // console.log(array[j], array[minIndex]);
+    // console.log("---------------------------------------------------------");
+    if (array[j] < array[minIndex]) {
+      minIndex = j;
     }
   }
-}
 
+  // console.log(minIndex, array[minIndex]);
+  if (minIndex != i) {
+    let temp = array[i];
+    array[i] = array[minIndex];
+    array[minIndex] = temp;
+  }
+}
 console.log(array);
