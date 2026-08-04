5fce019464ba3ac277227326a6c26be36963eba4
Sahil Yadav <sahil9082811@gmail.com>
2026-08-04 23:54:31 +0530

    Solving and understanding th selction short

--- ruff.js
+++ ruff.js
@@ -2,21 +2,17 @@ const array = [100, 45, 55, 25, 10, 98, 87, 11];
 
 for (let i = 0; i < array.length - 1; i++) {
   let minIndex = i;
-  // console.log(minIndex, "min");
-
   for (let j = i + 1; j < array.length; j++) {
-    // console.log(array[j], array[minIndex]);
-    // console.log("---------------------------------------------------------");
     if (array[j] < array[minIndex]) {
       minIndex = j;
     }
   }
 
-  // console.log(minIndex, array[minIndex]);
   if (minIndex != i) {
     let temp = array[i];
     array[i] = array[minIndex];
     array[minIndex] = temp;
   }
 }
-console.log(array);
+
+console.log(array, "Array");
