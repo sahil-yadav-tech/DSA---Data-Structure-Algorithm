efaf73c88123676d3108589eb4f5f9f28ed547ab
Sahil Yadav <sahil9082811@gmail.com>
2026-08-09 21:13:19 +0530

    Checking new object metho

---
+++ ruff.js
@@ -0,0 +1,15 @@
+let obj ={
+    name:"sahil"
+}
+
+let obj2 ={
+    names:"sahilsss"
+}
+
+let result = Object.assign({},obj, obj2)
+console.log(result, "Result");
+
+console.log(Object.keys(obj));
+console.log(Object.values(obj));
+console.log(Object.entries(obj));
+
