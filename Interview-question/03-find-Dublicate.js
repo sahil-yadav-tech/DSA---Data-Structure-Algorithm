c85a147a945f30269fbcb3dfb2aab0707ce6fbc4
Sahil Yadav <sahil9082811@gmail.com>
2026-08-12 23:06:34 +0530

    Finding Dublicate in array

--- Interview-question/03-find-Dublicate.js
+++ Interview-question/03-find-Dublicate.js
@@ -1,1 +1,16 @@
 // Find the duplicate in an array of integers.
+const array = [44, 64,64, 27,27,58,90, 58, 90, 10];
+let FindDublicate = []
+let oneElement  = []
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i]);
+    if(!oneElement.includes(array[i])){
+        oneElement.push(array[i])
+    }else{
+        FindDublicate.push(array[i])
+
+    }
+}
+console.log(FindDublicate);
+console.log(oneElement);
+
