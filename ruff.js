fa6c06d0036663554b78afa21cd297771e9a9b70
Sahil Yadav <sahil9082811@gmail.com>
2026-08-20 22:25:46 +0530

    Adding

--- ruff.js
+++ ruff.js
@@ -1,1 +1,9 @@
-// Converting
+// Check if a string is a palindrome.
+
+const checkPalindrome = (string) =>{
+    let check = string.split("")
+    console.log(check, "Check");
+    
+}
+
+checkPalindrome("user")
