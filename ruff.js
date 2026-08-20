42228b13047eeea769b02ea39839d14bf380ba38
Sahil Yadav <sahil9082811@gmail.com>
2026-08-20 23:00:15 +0530

    Solving reverse String without building method

--- ruff.js
+++ ruff.js
@@ -1,9 +1,14 @@
 // Check if a string is a palindrome.
+// Count the occurrences of a character in a string.
+// Find the longest substring without repeating characters.
+// Check if two strings are anagrams.
+// Implement string compression (e.g., 'aabcc' → 'a2b1c2').
 
-const checkPalindrome = (string) =>{
-    let check = string.split("")
-    console.log(check, "Check");
-    
-}
-
-checkPalindrome("user")
+//! everse a string without using built-in methods.
+// const string = "userData"
+// let reversString = ""
+// for (let index = 0; index < string.length; index++) {
+//     console.log(index, "Index");
+//     reversString = string[index]  +reversString
+// }
+// console.log(reversString);
