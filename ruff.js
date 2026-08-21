b317ee5b1f43fd7fb7f86e708c0ad268d3cbbc4c
Sahil Yadav <sahil9082811@gmail.com>
2026-08-22 01:09:35 +0530

    Checking switch loop

--- ruff.js
+++ ruff.js
@@ -1,25 +1,13 @@
-// Check if a string is a palindrome.
-// Count the occurrences of a character in a string.
-// Find the longest substring without repeating characters.
-// Check if two strings are anagrams.
-// Implement string compression (e.g., 'aabcc' → 'a2b1c2').
+let day = "monday";
 
-//! everse a string without using built-in methods.
-// const string = "userData"
-// let reversString = ""
-// for (let index = 0; index < string.length; index++) {
-//     console.log(index, "Index");
-//     reversString = string[index]  +reversString
-// }
-// console.log(reversString);
+switch (day) {
+  case "monday":
+    console.log("Start of the work week.");
+    break
 
-// Check if two strings are anagrams.
-// const CheckAnagram =(string1, string2) =>{
-//     const data1 = string1.split("").sort().join("")
-//     const data2 = string2.split("").sort().join("")
-//     console.log(data1, data2);
-// }
+  case "tuesday":
+    console.log("tuesday log");
 
-// CheckAnagram("listen", "silent")
-
-// Find the first non-repeated character in a string.
+  default:
+    console.log("Default");
+}
