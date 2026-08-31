6d2838dd98d9b0a2123c4cab3a78b35fcd6a1297
Sahil Yadav <sahil9082811@gmail.com>
2026-08-31 23:55:05 +0530

    finding dublicate value in array

--- ruff.js
+++ ruff.js
@@ -16,31 +16,45 @@
 // };
 
 // console.log(twoSum(array, target));
-// ===============================================================================
+//! ===============================================================================
 
 // Reverse a String
 // Reverse a string without using the built-in reverse() method.
 // Concept: Two pointers / loops
 
-let username = "sahilyadav";
-let reverseString = "";
-for (let i = 0; i < username.length; i++) {
-  reverseString =username[i] + reverseString ;
-}
-console.log(reverseString, "reverseString");
+// let username = "sahilyadav";
+// let reverseString = "";
+// for (let i = 0; i < username.length; i++) {
+//   reverseString =username[i] + reverseString ;
+// }
+// console.log(reverseString, "reverseString");
 
+//! ===============================================================================
 
-// Two Sum
-// Given an array and a target, find two numbers whose sum equals the target.
-// Concept: HashMap / Object / Map
-
-// Reverse a String
-// Reverse a string without using the built-in reverse() method.
-
-// Concept: Two pointers / loops
 // Find Duplicate Elements in an Array
 // Find all duplicate values in an array efficiently.
 // Concept: Set / Map / Hashing
+
+let array = [15, 50,15,50,20,18, 88, 66, 20, 90, 18];
+let dublicateValue = []
+let uniqueValue =[]
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if(uniqueValue.includes(array[i])){
+        dublicateValue.push(array[i])
+    }else{
+      uniqueValue.push(array[i])  
+    }
+}
+console.log(dublicateValue);
+console.log(uniqueValue);
+
+
+
+
+
+
 // Find the Largest and Second Largest Number
 // Find the largest and second-largest elements in an array in one pass.
 // Concept: Array traversal / optimization
