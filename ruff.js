12cc9dc2f9d1e6b9a7438bb2afed022fed412003
Sahil Yadav <sahil9082811@gmail.com>
2026-09-01 00:00:57 +0530

    Working second largest element in array

--- ruff.js
+++ ruff.js
@@ -35,29 +35,31 @@
 // Find all duplicate values in an array efficiently.
 // Concept: Set / Map / Hashing
 
-let array = [15, 50,15,50,20,18, 88, 66, 20, 90, 18];
-let dublicateValue = []
-let uniqueValue =[]
+// let array = [15, 50,15,50,20,18, 88, 66, 20, 90, 18];
+// let dublicateValue = []
+// let uniqueValue =[]
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    if(uniqueValue.includes(array[i])){
-        dublicateValue.push(array[i])
-    }else{
-      uniqueValue.push(array[i])  
-    }
-}
-console.log(dublicateValue);
-console.log(uniqueValue);
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+//     if(uniqueValue.includes(array[i])){
+//         dublicateValue.push(array[i])
+//     }else{
+//       uniqueValue.push(array[i])  
+//     }
+// }
+// console.log(dublicateValue);
+// console.log(uniqueValue);
 
-
-
-
-
-
+//! ===============================================================================
 // Find the Largest and Second Largest Number
 // Find the largest and second-largest elements in an array in one pass.
 // Concept: Array traversal / optimization
+
+
+
+
+
+//! ===============================================================================
 // Valid Parentheses
 // Given a string containing (), {}, and [], determine whether the brackets are properly balanced.
 // Concept: Stack
